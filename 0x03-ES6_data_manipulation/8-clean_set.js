27bb2c88200e3680beeb12587b832219a40f5c52
Natnael Bizuneh <106749223+NattyXO@users.noreply.github.com>
2023-04-27 10:02:11 +0300

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (startString && typeof startString === 'string') {
-    const strings = [];
-    for (const item of set) {
-      if (item.startsWith(startString)) {
-        strings.push(item.slice(startString.length));
-      }
+  let str = '';
+  for (const val of set) {
+    if (val.startsWith(startString)) {
+      str += val.slice(startString.length) + '-';
     }
-    return strings.join('-');
   }
-  return '';
+  return str.slice(0, -1);
 }
