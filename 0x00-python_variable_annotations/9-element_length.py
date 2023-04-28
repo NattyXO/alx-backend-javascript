c7cc8dfeb787e228ab93804f212eeb3b236c1c8e
Natnael Bizuneh <106749223+NattyXO@users.noreply.github.com>
2023-04-28 20:39:52 +0300

    Delete wrong path

--- 0x00-python_variable_annotations/9-element_length.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-""" Annotates function params and return values """
-
-from typing import List, Tuple, Sequence, Iterable
-
-
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """ Duck-typing """
-    return [(i, len(i)) for i in lst]
