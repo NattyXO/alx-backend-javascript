5f520cfca8494f8e78e06d007882e49166facdc6
Natnael Bizuneh <106749223+NattyXO@users.noreply.github.com>
2023-06-16 21:42:39 +0300

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
 
 function countStudents(filepath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filepath, 'utf-8', (err, res) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
+      if (err) return reject(new Error('Cannot load the database'));
 
       const headerArray = res.split(/\r?\n|\n/);
       const headers = headerArray[0].split(',');
 
+      // strip headers and convert to list of dicts
       const dictList = [];
-      for (let i = 1; i < headerArray.length; i++) {
-        const data = headerArray[i].split(',');
+      const noHeaderArray = headerArray.slice(1);
+      for (let i = 0; i < noHeaderArray.length; i += 1) {
+        const data = noHeaderArray[i].split(',');
         if (data.length === headers.length) {
           const row = {};
-          for (let j = 0; j < headers.length; j++) {
+          for (let j = 0; j < headers.length; j += 1) {
             row[headers[j].trim()] = data[j].trim();
           }
           dictList.push(row);
         }
       }
 
+      // count and collect first names of students per field
       let countCS = 0;
       let countSWE = 0;
       const studentsCS = [];
@@ -32,17 +31,17 @@ function countStudents(filepath) {
 
       dictList.forEach((element) => {
         if (element.field === 'CS') {
-          countCS++;
+          countCS += 1;
           studentsCS.push(element.firstname);
         } else if (element.field === 'SWE') {
-          countSWE++;
+          countSWE += 1;
           studentsSWE.push(element.firstname);
         }
       });
 
       const countStudents = countCS + countSWE;
 
-      resolve({
+      return resolve({
         countStudents,
         countCS,
         countSWE,
@@ -62,24 +61,24 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', async (req, res) => {
-  try {
-    const {
+  // call async function and collect needed variables
+  await countStudents(pathToDB)
+    .then(({
       countStudents,
       countCS,
       countSWE,
       studentsCS,
       studentsSWE,
-    } = await countStudents(path.join(pathToDB, 'database.csv'));
-
-    const text = 'This is the list of our students\n';
-    const total = `Number of students: ${countStudents}\n`;
-    const CS = `Number of students in CS: ${countCS}. List: ${studentsCS.join(', ')}\n`;
-    const SWE = `Number of students in SWE: ${countSWE}. List: ${studentsSWE.join(', ')}`;
-
-    res.status(200).send(text + total + CS + SWE);
-  } catch (err) {
-    res.status(404).send('Cannot load the database');
-  }
+    }) => {
+      const text = 'This is the list of our students\n';
+      const total = `Number of students: ${countStudents}\n`;
+      const CS = `Number of students in CS: ${countCS}. List: ${studentsCS.toString().split(',').join(', ')}\n`;
+      const SWE = `Number of students in SWE: ${countSWE}. List: ${studentsSWE.toString().split(',').join(', ')}`;
+      res.status(200).send(text + total + CS + SWE);
+    })
+    .catch(() => {
+      res.status(404).send('Cannot load the database');
+    });
 });
 
 app.listen(port);
