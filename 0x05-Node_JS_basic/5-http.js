1b3a38e5aa2c6728022e2661e740c60cf030d724
Natnael Bizuneh <106749223+NattyXO@users.noreply.github.com>
2023-06-18 22:48:14 +0300

    11Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,24 +1,51 @@
-const express = require('express');
+const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = express();
+const args = process.argv.slice(2);
+const DATABASE = args[0];
+
+const hostname = '127.0.0.1';
 const port = 1245;
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
+const server = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-app.get('/students', async (req, res) => {
-  try {
-    const students = await countStudents(req.query.database);
-    res.send(`This is the list of our students\nNumber of students: ${students.countStudents}\nNumber of students in CS: ${students.countCS}. List: ${students.studentsCS.join(', ')}\nNumber of students in SWE: ${students.countSWE}. List: ${students.studentsSWE.join(', ')}`);
-  } catch (error) {
-    res.send('Error: Cannot load the database');
+  const { url } = req;
+
+  if (url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    try {
+      const students = await countStudents(DATABASE);
+      const response = generateStudentsResponse(students);
+      res.end(response);
+    } catch (error) {
+      res.statusCode = 500;
+      res.end('Error: Cannot load the database');
+    }
+  } else {
+    res.statusCode = 404;
+    res.end('404 Not Found');
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-module.exports = app;
+function generateStudentsResponse(students) {
+  const {
+    countStudents,
+    countCS,
+    countSWE,
+    studentsCS,
+    studentsSWE,
+  } = students;
+
+  const response = `This is the list of our students\nNumber of students: ${countStudents}\nNumber of students in CS: ${countCS}. List: ${studentsCS.join(', ')}\nNumber of students in SWE: ${countSWE}. List: ${studentsSWE.join(', ')}`;
+  
+  return response;
+}
+
+module.exports = server;
